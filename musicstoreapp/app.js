5c7f913d08c805d9906170e59f86b43c7982f298
Sergio <uo276341@uniovi.es>
2022-04-01 00:00:27 +0200

    SDI-2122-302-Ejercicio complementario 2

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -12,7 +12,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-require("./routes/songs.js")(app);
+
+const {MongoClient} = require("mongodb");
+const url = 'mongodb+srv://admin:Marvel123@tiendamusica.hhkbe.mongodb.net/tiendamusica?retryWrites=true&w=majority';
+app.set('connectionStrings', url);
+require("./routes/songs.js")(app, MongoClient);
 require("./routes/authors.js")(app);
 
 // view engine setup
